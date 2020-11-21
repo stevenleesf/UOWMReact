4134aeb6ad0c0bcd057cf86df98e28cd3d596ce1
Lee Soon Fatt <lee.soonfatt98@gmail.com>
2020-11-21 13:25:07 +0800

    Updates

---
+++ precache-manifest.e26636acc86c7fea7ceb1dcbd9851920.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca31445e972493b7b4a5f81db77219f0",
+    "url": "/UOWMReact/index.html"
+  },
+  {
+    "revision": "9cc9d6be4d58b68aebf2",
+    "url": "/UOWMReact/static/css/2.55ce7b8a.chunk.css"
+  },
+  {
+    "revision": "5b6b23b1b8f298f390f7",
+    "url": "/UOWMReact/static/css/main.e7f60d59.chunk.css"
+  },
+  {
+    "revision": "9cc9d6be4d58b68aebf2",
+    "url": "/UOWMReact/static/js/2.47af72dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/UOWMReact/static/js/2.47af72dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6b23b1b8f298f390f7",
+    "url": "/UOWMReact/static/js/main.920ba147.chunk.js"
+  },
+  {
+    "revision": "5f3a572c760c5bc8e0df",
+    "url": "/UOWMReact/static/js/runtime-main.63d1ead4.js"
+  },
+  {
+    "revision": "206ff732ca268905140bfcb1ff255b29",
+    "url": "/UOWMReact/static/media/banner.206ff732.jpg"
+  },
+  {
+    "revision": "0eed37d6df15d4105e3d643ce2472d05",
+    "url": "/UOWMReact/static/media/kdu.0eed37d6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/UOWMReact/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/UOWMReact/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/UOWMReact/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/UOWMReact/static/media/slick.f97e3bbf.svg"
+  }
+]);
