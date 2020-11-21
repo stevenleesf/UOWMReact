078d1e5e92acc91fe4567d3466aa8960bec31972
Lee Soon Fatt <lee.soonfatt98@gmail.com>
2020-11-21 11:07:06 +0800

    Updates

---
+++ precache-manifest.6e1b14334115cfe500c5a3407db30f16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5a9db0056e814c14086105177f6e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4da99b3e911e9da60f98",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "66d942a1b603d570eccf",
+    "url": "/static/css/main.34b2587d.chunk.css"
+  },
+  {
+    "revision": "4da99b3e911e9da60f98",
+    "url": "/static/js/2.47af72dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.47af72dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66d942a1b603d570eccf",
+    "url": "/static/js/main.67cbd7fb.chunk.js"
+  },
+  {
+    "revision": "68f6ed2cee41db273186",
+    "url": "/static/js/runtime-main.6b3d174d.js"
+  },
+  {
+    "revision": "206ff732ca268905140bfcb1ff255b29",
+    "url": "/static/media/banner.206ff732.jpg"
+  },
+  {
+    "revision": "0eed37d6df15d4105e3d643ce2472d05",
+    "url": "/static/media/kdu.0eed37d6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
